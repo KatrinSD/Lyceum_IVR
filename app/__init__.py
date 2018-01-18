aa687decbc1917e1e73c3587e95a21047af94629
KatrinSD <sovetochka@gmail.com>
2018-01-18 12:18:45 +0300

    Init flask-uploads on app init

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 from flask_redis import FlaskRedis
-
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 app.config["REDIS_URL"] = "redis://localhost:6379"
@@ -14,6 +14,10 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 redis = FlaskRedis(app)
+
+photos = UploadSet("photos", IMAGES)
+app.config["UPLOADED_PHOTOS_DEST"] = "static/img"
+congigure_uploads(app, photos)
 
 from app.tags import TagsDriver
 
